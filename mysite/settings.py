c30e84c8eac32f7ebfeda14b55d2825374a75d4f
rafaelurben <github@rafaelurben.ch>
2023-06-18 20:35:25 +0200

    Updated to Django 4.2.2

--- mysite/settings.py
+++ mysite/settings.py
@@ -293,14 +293,17 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'mysite' / 'static'
 ]
 
-
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
